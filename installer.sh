77aaaef572c08fc79000b701f1207575b8be5402
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-20 01:47:38 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,7 @@
 wait
 
 opkg install --force-overwrite  
-"https://raw.githubusercontent.com/tarekzoka/D/main/enigma2-plugin-extensions-xcplugin-forever_1.7_all.ipk"
+https://raw.githubusercontent.com/tarekzoka/D/main/enigma2-plugin-extensions-xcplugin-forever_1.7_all.ipk
 wait
 opkg install --force-overwrite /tmp/*.ipk
 wait
