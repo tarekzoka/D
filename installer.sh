840d830a1476624a415e97b3581bddfc1faf1a10
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-20 13:17:34 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,19 +1,31 @@
+  
 #!/bin/sh
-
-# 
-# SCRIPT : DOWNLOAD AND INSTALL D #
-# 
-# Command: wget https://raw.githubusercontent.com/tarekzoka/D/main/installer.sh -O - | /bin/sh #
-
+#
+# SCRIPT : DOWNLOAD AND INSTALL D
+#
+###########################################
+#command: wget https://raw.githubusercontent.com/tarekzoka/D/main/installer.sh -O - | /bin/sh
+ #
+MY_EM="*********************************************************************************"
+wget -O /var/volatile/tmp/xcplugin-forever_1.7_all.ipk "https://raw.githubusercontent.com/emilnabil/D/main/xcplugin-forever_1.7_all.ipk"
 wait
-#!/bin/sh
-
-# 
-opkg install --force-overwrite  
-https://raw.githubusercontent.com/tarekzoka/D/main/enigma2-plugin-extensions-xcplugin-forever_1.7_all.ipk
-wait
+echo $MY_EM
+######      INSTALL PLUGIN Xcplugin ###
 opkg install --force-overwrite /tmp/*.ipk
 wait
-sleep 2;
+echo $MY_EM
+#######   REMOVE PACKAGE FROM TMP ###
+rm -r /var/volatile/tmp/xcplugin-forever_1.7_all.ipk 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo $MY_EM
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY TARE_ZOK " 
+echo "======================================================"
+echo "       YOUR DEVICE WELL RESTART Now " 
+echo "****************************************"
+wait 3;
+init 4
+init 3
 exit 0
 
