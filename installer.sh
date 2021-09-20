0529c780347c6a7b7fff4d07128c60516d39c26a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-20 14:38:00 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # SCRIPT : DOWNLOAD AND INSTALL D
 #
 ###########################################
-#command: wget https://raw.githubusercontent.com/tarekzoka/D/main/installer.sh -O - | /bin/sh
+#command: wget https://raw.githubusercontent.com/tarekhnfy/D/main/installer.sh -O - | /bin/sh
  opkg install --force-overwrite  https://github.com/tarekzoka/D/قraw/main/enigma2-plugin-extensions-xcplugin-forever_1.7_all.ipk
 wait
 sleep 2;
